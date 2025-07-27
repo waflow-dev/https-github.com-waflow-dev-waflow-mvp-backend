4e445cff967b32c17ebb726e91ba858a971a67b5
Ankush9954 <ankush9954@gmail.com>
2025-07-27 13:22:16 +0530

    Debugging

--- services/application/routes/index.js
+++ services/application/routes/index.js
@@ -61,7 +61,7 @@ router.patch(
 );
 
 // Add note for clarification
-router.post("/note/:appId", authenticateToken, addNote);
+router.post("/note/:appId", addNote);
 
 router.post("/visa-member/:customerId", authenticateToken, addVisaMember);
 

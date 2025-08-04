0cf5dd6714bdf802a7ed0959d7b239a5d60229df
guptaRishi00 <rishigupta54066@gmail.com>
2025-08-04 16:59:20 +0530

    added cors logic

--- services/user/routes/index.js
+++ services/user/routes/index.js
@@ -52,7 +52,7 @@ router.get(
 
 router.get("/customers", authenticateToken, getAllCustomers);
 
-router.get("/agents", authenticateToken, authorizeRoles("admin"), getAllAgents);
+router.get("/agents", authenticateToken, authorizeRoles("admin", "manager"), getAllAgents);
 
 router.put(
   "/agent/:agentId",

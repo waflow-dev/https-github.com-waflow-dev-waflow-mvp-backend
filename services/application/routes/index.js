851845c597bb61efebcda06194c495c58e0c5188
Ankush9954 <ankush9954@gmail.com>
2025-08-11 13:17:05 +0530

    Debugging

--- services/application/routes/index.js
+++ services/application/routes/index.js
@@ -49,7 +49,7 @@ router.patch(
 
 // Update application step status — agent/admin
 router.patch(
-  "/stepStatus/:customerId",
+  "/step-status/:customerId",
   authenticateToken,
   authorizeRoles("agent", "admin"),
   updateStepStatus
